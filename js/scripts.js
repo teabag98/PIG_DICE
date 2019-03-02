dee923d992546fd258683c469965761c9fc759c6
eric <eric.kingoo2014@gmail.com>
2019-03-02 16:12:39 +0300

    add custom alert box

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,76 @@
 //business logic
-var randomNumber = Math.floor(Math.random() * 6) + 1
 
-function PigDice(addingUp(), randomNumber) {
-  this.permanentScore = addingUp();
-  this.temporaryScore = randomNumber;
+
+function PigDice() {
+  this.permanentScore = 0;
+  this.temporaryScore = 0;
   this.hold;
-  this.play;
+  this.roll;
 
 
 }
+
+var arrayNumbers = [];
+
+var randomNumber;
+
+PigDice.prototype.generateNumber = function() {
+  randomNumber = Math.floor(Math.random() * 6) + 1
+  arrayNumbers.push(randomNumber);
+  for (i = 1; i <= randomNumber.length; i++) {
+alert("hey"+i);
+  }
+}
+generateNumber();
 
 
 
 //when the outcome is 1
-PigDice.prototype.addingUp = function(num) {
-  if (this.temporaryScore === 1) {
-    alert("your turn is over. Your total score is 0");
-    this.permanentScore = 0;
+// PigDice.prototype.addingUp = function(i) {
+//   if (arrayNumbers[i] === 1) {
+//     alert("your turn is over. Your total score is 0");
+//     this.permanentScore = 0;
+//
+//   } else if (arrayNumbers[i] === 2) {
+//     return this.permanentscore += 10;
+//
+//
+//   } else if (arrayNumbers[i]=== 3) {
+//     return this.permanentScore += 15;
+//   } else if (arrayNumbers[i] === 4) {
+//     return this.permanentScore += 15;
+//   } else if (arrayNumbers[i] === 5) {
+//     return this.permanentScore += 20
+//   } else {
+//     return this.permanentScore += 20
+//   }
+//
+// }
+//
 
-  } else if(this.temporaryScore === 2) {
-    this.permanentscore +=10;
-
-  }
-  else if(this.temporaryScore===3){
-    this.permanentScore +=15;
-  }
-  else if(this.temporaryScore===4){
-    this.permanentScore +=15;
-  }
-  else if(this.temporaryScore=== 5){
-    this.permanentScore +=20
-  }
-  else{
-    this.permanentScore+=20
-  }
-}
-
-//objects
-var firstGamer = new PigDice();
-var secondGamer = new PigDice();
-
-
-
-
-
-
-
-
-
-
-
-
-//user logic
-$(document).ready(function() {
-  var gamerOne = $("input#gamer1").val();
-  var gamerTwo = $("input#gamer2").val();
-  var gamerThree = $("input#gamer3").val();
-  var gamerFour = $("input#gamer4").val();
-  $("button#play").click(function() {
-    $(".jumbotron").slideUp(800, function() {
-      // $("h1.player-one").append(gamerOne);
-      // $("h1.player-two").text(gamerThree);
-
-    });
-
-    $("div.header").show();
-  });
-  $("button#roll").click(function() {
-$("#tester").text(firstGamer.addingUp(randomNumber));
-  });
-});
+// //objects
+// var firstGamer = new PigDice();
+// var secondGamer = new PigDice();
+//
+//
+// var accumulativeScore = firstGamer.permanentScore.generateNumber();
+//
+// //user logic
+// $(document).ready(function() {
+//   var gamerOne = $("input#gamer1").val();
+//   var gamerTwo = $("input#gamer2").val();
+//   var gamerThree = $("input#gamer3").val();
+//   var gamerFour = $("input#gamer4").val();
+//   $("button#play").click(function() {
+//     $(".jumbotron").slideUp(800, function() {
+//       // $("h1.player-one").append(gamerOne);
+//       // $("h1.player-two").text(gamerThree);
+//
+//     });
+//
+//     $("div.header").show();
+//   });
+  // $("button#roll").click(function() {
+  //   alert(firstGamer.roll.generateNumber());
+//   });
+// });
