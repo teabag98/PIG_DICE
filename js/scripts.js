8e3e9c696da406d8b74131e1ed741b950f13a0c8
eric <eric.kingoo2014@gmail.com>
2019-03-04 12:15:56 +0300

    edit header backgroung and play button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,7 @@ Dice.prototype.hold = function() {
   this.total += this.rollscore;
 }
 
+//update total score when player roll
 function updateFirstScore() {
   var total = playerOne.total += playerOne.rollScore;
   document.getElementById("total-score").innerHTML = total;
@@ -74,7 +75,7 @@ function updateFirstScore() {
 
 function updateSecondScore() {
   var total = playerTwo.total += playerTwo.rollScore;
-  document.getElementById("total-score2").innerHTML = total;
+  document.getElementById("total-score2").innerHTML = total*2;
   playerTwo.rollScore = 0;
   document.getElementById("current-score2").innerHTML = playerOne.rollScore;
 }
@@ -95,19 +96,12 @@ function playerTwoSwitch() {
   document.getElementById("player1-hold").disabled = false;
 }
 
-// Dice.prototype.winner = function() {
-//   if (this.total >= 10) {
-//     alert("congratulations");
-//   }
-//   else{
-//
-//   }
 
 //checks winner
 function winner() {
-  if (playerOne.total >= 100) {
+  if (playerOne.total >= 50) {
     alert(gamerOne + " you are winner")
-  } else if (playerTwo.total >= 100) {
+  } else if (playerTwo.total >= 50) {
     alert(gamerTwo + " you are a winner")
   }
 }
@@ -149,100 +143,3 @@ $(document).ready(function() {
     $(".player1-results").removeClass("opacity");
   });
 });
-
-
-
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-// //user logic
-// $(document).ready(function() {
-//   var gamerOne = $("input#gamer1").val();
-//   var gamerTwo = $("input#gamer2").val();
-//   var gamerThree = $("input#gamer3").val();
-//   var gamerFour = $("input#gamer4").val();
-//   $("button#play").click(function() {
-//     $(".jumbotron").slideUp(800, function() {
-//       // $("h1.player-one").append(gamerOne);
-//       // $("h1.player-two").text(gamerThree);
-//
-//     });
-//
-//     $("div.header").show();
-//   });
-//
-// });
-// // });
-
-
-
-
-
-
-
-
-
-//business logic
-
-//
-// function PigDice() {
-//   this.permanentScore = 0;
-//   this.temporaryScore = 0;
-//   this.hold;
-//   this.roll;
-//
-//
-// }
-//
-// var arrayNumbers = [];
-//
-// var randomNumber;
-//
-// PigDice.prototype.generateNumber = function() {
-//   randomNumber = Math.floor(Math.random() * 6) + 1
-//   arrayNumbers.push(randomNumber);
-//   for (i = 1; i <= randomNumber.length; i++) {
-//     return this += [i];
-//   }
-// }
-
-
-
-
-//when the outcome is 1
-// PigDice.prototype.addingUp = function(i) {
-//   if (arrayNumbers[i] === 1) {
-//     alert("your turn is over. Your total score is 0");
-//     this.permanentScore = 0;
-//
-//   } else if (arrayNumbers[i] === 2) {
-//     return this.permanentscore += 10;
-//
-//
-//   } else if (arrayNumbers[i]=== 3) {
-//     return this.permanentScore += 15;
-//   } else if (arrayNumbers[i] === 4) {
-//     return this.permanentScore += 15;
-//   } else if (arrayNumbers[i] === 5) {
-//     return this.permanentScore += 20
-//   } else {
-//     return this.permanentScore += 20
-//   }
-//
-// }
-//
-
-// //objects
-// var firstGamer = new PigDice();
-// var secondGamer = new PigDice();
-//
-//
-// var accumulativeScore = firstGamer.permanentScore.generateNumber();
-//
